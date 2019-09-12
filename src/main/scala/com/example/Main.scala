e62701060ac33820dc9add79d715445ece6be526
Ashwin Raja <ashwin.craja@gmail.com>
2019-09-11 19:52:53 -0700

    Update Main.scala

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -3,7 +3,5 @@ package com.example
 object Main extends App {
   def excite(s: String): String = s"$s!"
 
-  override def main(): Unit = {
-    println(excite("Hello, world"))
-  }
+  println(excite("Hello, world"))
 }
